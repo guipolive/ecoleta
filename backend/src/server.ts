ed1ef5b3815e96f4ed2e22eaa70168867030135f
Guilherme Oliveira <gui.passosdeoliveira@gmail.com>
2020-06-15 18:23:53 -0300

    add images

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -5,6 +5,7 @@
 */
 
 import express, { response } from 'express';
+import path from 'path';
 import routes from './routes';
 
 const app = express();
@@ -12,5 +13,7 @@ const app = express();
 app.use(express.json()); // permite que o express passe a entender corpo de requisição com formatos JSON
 
 app.use(routes); // faz com que o express use as rotas que separamos em outro arquivo de rotas
+
+app.use('/uploads', express.static(path.resolve(__dirname, '..', 'temp', 'uploads')));
 
 app.listen(3333);
